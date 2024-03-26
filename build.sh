47b2267d175901f2bf1f9b24bb47065cf3220b00
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2024-03-26 22:55:17 +0900

    ビルドスクリプトを削除

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 rm ./output/GenBitmap.ttf
 java -jar ./bin/BitsNPicas.jar convertbitmap -f ttf -o ./output/GenBitmap.ttf ./GenBitmap.kbitx
+rm ~/Libary/Fonts/GenBitmap.ttf
+cp ./output/GenBitmap.ttf ~/Libary/Fonts/GenBitmap.ttf
