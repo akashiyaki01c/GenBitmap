6eca7464c4b607f1434b39512ef2d5f859c0ce39
Xifire <95431075+akashiyaki01c@users.noreply.github.com>
2024-03-27 20:16:12 +0900

    人偏を修正

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm ./output/GenBitmap.ttf
-java -jar ./bin/BitsNPicas.jar convertbitmap -f ttf -o ./output/GenBitmap.ttf ./GenBitmap.kbitx
-rm ~/Libary/Fonts/GenBitmap.ttf
-cp ./output/GenBitmap.ttf ~/Libary/Fonts/GenBitmap.ttf
